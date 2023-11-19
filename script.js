b953f79ccf382dd4fae0b8cca4b80221a56d6714
Gulshan Kumar <74947700+gulshank99@users.noreply.github.com>
2023-11-20 01:46:20 +0530

    Added accomplishment section. and top contries . Workred on responsiveness of mentor page , top college page, accomplishment page.

--- script.js
+++ script.js
@@ -1,3 +1,70 @@
+ // Top university
+var multipleCardCarousel = document.querySelector("#carouselExampleControlss");
+
+if (window.matchMedia("(min-width: 576px)").matches) {
+  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+    interval: false,
+  });
+  var carouselWidth = $(".carousel-innerPart")[0].scrollWidth;
+  var cardWidth = $(".carousel-item-in").width();
+  var scrollPosition = 0;
+  $("#carouselExampleControlss .coro-next").on("click", function () {
+    if (scrollPosition < carouselWidth - cardWidth * 3) {
+      scrollPosition += cardWidth;
+      $("#carouselExampleControlss .carousel-innerPart").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+  $("#carouselExampleControlss .coro-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $("#carouselExampleControlss .carousel-innerPart").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+} else {
+  $(multipleCardCarousel).addClass("slide-edge");
+}
+
+
+var demoButtons;
+
+function start () {
+  demoButtons = document.querySelectorAll('.js-modify');
+  for (var i = 0; i < demoButtons.length; i++) {
+    demoButtons[i].addEventListener ('click', toggleEffect);
+  }
+  var saveButtons = document.querySelectorAll ('.js-save');
+  for (var i = 0; i < saveButtons.length; i++) {
+    saveButtons[i].addEventListener ('click', toggleActive);
+  }
+  
+}
+
+function toggleEffect () {
+  var target = document.querySelector (this.dataset.target);
+      target.dataset.effect = this.dataset.effect;
+  
+  for (var i= 0; i < demoButtons.length; i++) {
+    demoButtons[i].classList.remove ('active');
+  }
+  
+  toggleActive.call (this);
+}
+
+function toggleActive () {
+  this.classList.toggle ('active');
+}
+window.addEventListener ('load', start);
+
+
+
+
+
 // Accomplishment section
 
 var swiper = new Swiper(".mySwiper", {
@@ -36,65 +103,6 @@ var swiper = new Swiper(".mySwiper", {
     
   });
   
-
-
-const initSlider = () => {
-    const imageList = document.querySelector(".slider-wrapper .image-list");
-    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
-    const sliderScrollbar = document.querySelector(".container .slider-scrollbar");
-    const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
-    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
-    
-    // Handle scrollbar thumb drag
-    scrollbarThumb.addEventListener("mousedown", (e) => {
-        const startX = e.clientX;
-        const thumbPosition = scrollbarThumb.offsetLeft;
-        const maxThumbPosition = sliderScrollbar.getBoundingClientRect().width - scrollbarThumb.offsetWidth;
-        
-        // Update thumb position on mouse move
-        const handleMouseMove = (e) => {
-            const deltaX = e.clientX - startX;
-            const newThumbPosition = thumbPosition + deltaX;
-            // Ensure the scrollbar thumb stays within bounds
-            const boundedPosition = Math.max(0, Math.min(maxThumbPosition, newThumbPosition));
-            const scrollPosition = (boundedPosition / maxThumbPosition) * maxScrollLeft;
-            
-            scrollbarThumb.style.left = `${boundedPosition}px`;
-            imageList.scrollLeft = scrollPosition;
-        }
-        // Remove event listeners on mouse up
-        const handleMouseUp = () => {
-            document.removeEventListener("mousemove", handleMouseMove);
-            document.removeEventListener("mouseup", handleMouseUp);
-        }
-        // Add event listeners for drag interaction
-        document.addEventListener("mousemove", handleMouseMove);
-        document.addEventListener("mouseup", handleMouseUp);
-    });
-    // Slide images according to the slide button clicks
-    slideButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            const direction = button.id === "prev-slide" ? -1 : 1;
-            const scrollAmount = imageList.clientWidth * direction;
-            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
-        });
-    });
-     // Show or hide slide buttons based on scroll position
-    const handleSlideButtons = () => {
-        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
-        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
-    }
-    // Update scrollbar thumb position based on image scroll
-    const updateScrollThumbPosition = () => {
-        const scrollPosition = imageList.scrollLeft;
-        const thumbPosition = (scrollPosition / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
-        scrollbarThumb.style.left = `${thumbPosition}px`;
-    }
-    // Call these two functions when image list scrolls
-    imageList.addEventListener("scroll", () => {
-        updateScrollThumbPosition();
-        handleSlideButtons();
-    });
-}
-window.addEventListener("resize", initSlider);
-window.addEventListener("load", initSlider);
+  
+  
+ 
